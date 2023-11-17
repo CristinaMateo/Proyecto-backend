633114ea68588a6e577f08da347862466378e9c4
Cristina Mateo <cris.mateo.99@gmail.com>
2023-11-17 14:15:58 +0100

    view login y ruta sin autenticacion

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
 
 //habilita recepción objetos
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
-//middlewares
-// const $ = require('./middlewares/');
-// const % = require('./middlewares/');
-// const & = require('./middlewares/');
+//middelwares
+const error404 = require('./middlewares/error404')
+const morgan = require('./middlewares/morgan')
+
+
+// Logger
+app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
+
+//rutas
+const loginRoutes = require("./routes/login.routes")
+
+
+//Rutas Template
+app.use('/', loginRoutes);
+
 
 //configuración plantilla pug
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-//habilita
-// app.use($);
-// app.use('*',%);
-// app.use(&(':method :host :status :param[id] - :response-time ms :body'));
 
-// const ? = require('./routes/');
-// const ! = require('./routes/');
-
-//ruta api sin el /api seria para la web
-// app.use('/api', ?);
-// app.use('/api', !);
-
-app.get('/', (req, res) => {
-    res.send('Home')
-});
-
-// app.get('/first_template', function(req, res){
-//     res.render('first_view.pug');
-//   });
-  
-//   app.get('/dynamic_view', function(req, res){
-//     // Llamada a otra API o BBDD
-//     // Procesar datos y preparar objeto
-//     res.render('dynamic', {
-//        name: "The bridge - FullStack", 
-//        url:"https://thebridge.tech"
-//     });
-//   });
+//para rutas no existentes
+app.use('*',error404)
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
